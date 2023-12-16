22b8e55d6f25f2d2d51d258bcc7779aeac556e97
Lior Rotberg <Exolite@gmail.com>
2023-12-16 02:00:41 +0200

    Removed unneeded delay

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,11 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import delay from "delay";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import IssueActions from "./IssueActions";
 import Link from "../components/Link";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(1000);
 
   return (
     <div>
