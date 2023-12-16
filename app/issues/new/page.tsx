366c9555d3c07106c16277c01495fd73aae322ca
Lior Rotberg <Exolite@gmail.com>
2023-12-16 02:29:27 +0200

    Refactor: Organize  import statements

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,14 @@
 "use client";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+import { ErrorMessage, Spinner } from "@/app/components";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import dynamic from "next/dynamic";
 import { z } from "zod";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
