b8a7df991180979c56123fcaf1d158fbf52b57b0
Lior Rotberg <Exolite@gmail.com>
2023-12-18 21:49:31 +0200

    dd the login and logout links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import { Container, Theme } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
-import "./theme-config.css";
+import AuthProvider from "./auth/Provider";
 import "./globals.css";
+import "./theme-config.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -16,12 +17,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={inter.variable}>
-        <Theme accentColor="violet">
-          <NavBar />
-          <main className="p-5">{children}</main>
-        </Theme>
-      </body>
+      <AuthProvider>
+        <body className={inter.variable}>
+          <Theme accentColor="violet">
+            <NavBar />
+            <main className="p-5">{children}</main>
+          </Theme>
+        </body>
+      </AuthProvider>
     </html>
   );
 }
