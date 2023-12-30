f60924b4d33a53048e9b86bbc0db788a3b922628
Lior Rotberg <Exolite@gmail.com>
2023-12-30 16:49:45 +0200

    Fix clear button not working issue

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -1,14 +1,22 @@
+"use client";
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssueActions = () => {
+  const router = useRouter();
+
+  const clearFilter = () => {
+    router.push("/issues/list");
+  };
+
   return (
     <Flex>
       <Flex gap="2" mr="6">
         <IssueStatusFilter />
-        <Button variant="surface">
-          <Link href="/issues/list">Clear</Link>
+        <Button variant="surface" onClick={clearFilter}>
+          Clear
         </Button>
       </Flex>
       <Button>
